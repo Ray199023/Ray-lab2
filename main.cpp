6cf5bb31f5aecc0e64fdd49b9f3996bec50caeaa
Ray199023 <raykierans@gmail.com>
2023-07-08 10:22:01 +0000

    start of programme
    Signed-off-by: Ray199023 <raykierans@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+
+//0,1,1,2,3,5,8,13.....
+
+int main()
+{
+    int max_term = 10;
+    int term_1 = 0;
+    int term_2 = 1;
+    int next_term = 0;
+    for(int i=1; i<=max)
+    while (true) {
+
+    }
+}
+
